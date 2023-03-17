e9bd069b852f4469758290a500a02ea37c46396a
Lianne Turnbull <lianne.turnbull@gmail.com>
2023-03-17 20:04:24 +0000

    Add prettier

---
+++ src/components/MainPage.tsx
@@ -0,0 +1,13 @@
+import { Grid, Heading } from "@chakra-ui/react";
+import { ComponentType } from "react";
+
+const MainPage: ComponentType = () => {
+  return (
+    <Grid minH="100vh">
+      <Heading>Guild Wars 2</Heading>
+      <Heading>Envoy Armor Guide</Heading>
+    </Grid>
+  );
+};
+
+export default MainPage;
